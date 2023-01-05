972d9e7f53dac1e8d1a589d0c44b3bb8bb3bdf7a
oNavShaHo <106837111+oNavShaHo@users.noreply.github.com>
2023-01-05 22:03:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+
 import './App.css';
+import w1 from './i/w1.png';
+import w2 from './i/w2.png';
+import w4 from './i/w4.png';
+import w5 from './i/w5.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+
+
+<p><br/></p>
+<p className="heading">News 1</p>
+<p><br/></p>
+<h1>PayPal enables transfer of digital currencies to external wallets</h1>
+<p>The move comes after nearly two years since PayPal enabled users to buy and sell crypto on its platform.</p>
+<p><img src={w1} width="524.9711934156379" height="217.4320987654321" alt='w1'/></p>
+<p><br/></p>
+<p>&nbsp;</p>
+<p>After rolling out the ability to buy and sell crypto on its platform in October 2020, PayPal is finally&nbsp;<a href="https://newsroom.paypal-corp.com/2022-06-07-PayPal-Users-Can-Now-Transfer-Send-and-Receive-Bitcoin-Ethereum-Bitcoin-Cash-and-Litecoin">allowing</a> users the ability to natively transfer, send and receive digital assets between PayPal and other wallets and exchanges. As of Tuesday, the feature is available to select U.S. users, with the feature expanding to all eligible U.S. users in the coming weeks. The first batch of supported coins consists of Bitcoin&nbsp;</p>
+<p><a href="https://cointelegraph.com/bitcoin-price"><div className='b'> BTC&nbsp; </div>$16,704</a>, Ether &nbsp;<a href="https://cointelegraph.com/ethereum-price"><div className='b'>ETH&nbsp;</div>$1,217</a>, Bitcoin Cash&nbsp;<a href="https://cointelegraph.com/bitcoin-cash-price-index"><div className='b'> BCH</div>&nbsp;$100</a>, and <div className='b'> Litecoin</div>&nbsp;<a href="https://cointelegraph.com/ltc-price-index">LTC&nbsp;$74.58</a>. &nbsp;</p>
+<p>In addition, customers who transfer their crypto onto PayPal can spend it via Checkout at millions of merchant terminals. The company has been granted a full Bitlicense by the New York Department of Financial Services for the conduct.</p>
+<p>Users would simply need to log in to their accounts and enter the crypto section of the application to start transferring coins. Users are generally required to complete a one-time ID verification before the procedure.</p>
+<p>Crypto transfers to recipients outside of PayPal would incur a network fee based on their respective blockchains, but transfers between PayPal users will not incur such fees. To protect users&apos; privacy, the firm generates a new recipient address for each transaction into one&apos;s PayPal account. PayPal will also not charge fees for incoming transfers,</p>
+<p>The company is also working to&nbsp;<a href="https://cointelegraph.com/news/wef-2022-paypal-looks-to-embrace-all-possible-crypto-and-blockchain-services"><div className='black'>integrate</div></a> other forms of cryptocurrency services, such as central bank digital currencies, to boost its digital footprint. It is also exploring the possibility of launching&nbsp;<a href="https://cointelegraph.com/news/paypal-stablecoin-what-it-could-mean-for-payments"><div className='black'>its own stablecoin, dubbed &quot;PayPal Coin</div></a>.&quot; The discovery came after a developer found evidence of such a stablecoin within the source code of the company&apos;s iPhone app.&nbsp;</p>
+<p className="heading">News 2&nbsp;</p>
+<p><br/></p>
+<p>Italian Parliament Approves 26% Crypto-Gains Tax in 2023 Budget</p>
+<p>Prime Minister Giorgia Meloni&apos;s new bill also includes an incentive for those declaring crypto for tax purposes.<br/><br/><br/></p>
+<p>Crypto traders in Italy will be subject to a 26% capital-gains tax starting in 2023, according to a new budget that won parliamentary approval on Thursday.</p>
+<p>Italian Prime Minister Giorgia Meloni&apos;s 2023 expansionary budget &ndash; which was completed in a rush before the end of the year &ndash; features 21 billion euros ($22.3 billion) in tax breaks to assist businesses and households facing the energy crisis,&nbsp;<img src={w2} width="602" height="301" alt='w2'/>Reuters&nbsp;<a href="https://www.reuters.com/markets/europe/italys-parliament-gives-final-approval-governments-2023-budget-2022-12-29/">reported</a>.</p>
+<p>In Italy, where crypto&nbsp;<a href="https://www.coindesk.com/policy/2022/10/05/italy-has-a-registry-of-73-unvetted-crypto-firms/">remains largely unregulated</a>, the 387-page budget legitimizes crypto assets by defining them as &quot;a digital representation of value or rights, which can be transferred and stored electronically, using the technology of distributed ledger or similar technology.&quot;</p>
+<p>Italy&apos;s (and most recently&nbsp;<a href="https://www.coindesk.com/policy/2022/10/10/portuguese-proposal-would-enact-taxes-on-crypto-transfers-capital-gains/">Portugal&apos;s</a>) move to introduce a capital-gains tax on crypto comes ahead of the implementation of the European Union&apos;s&nbsp;<a href="https://www.coindesk.com/policy/2022/11/04/eu-delays-vote-on-mica-crypto-legislation-until-february/">Markets in Crypto Assets (MiCA)</a> regulation that promises a licensing frameworks and stringent operating requirements for crypto-service providers in the 27-member bloc.</p>
+<p>The 26% rate applies to gains from crypto trading if they exceed 2,000 euros per tax period. As an incentive for declaring crypto profits, the new bill also sets a &quot;substitute income tax&quot; for investors at 14% of the value of the assets held as of Jan. 1, 2023, instead of the cost at the time of purchase.</p>
+<p>According to the new rules, losses from crypto investments can be deducted from profits and be carried forward.</p>
+<p>Investors, however, may require some additional guidance on what qualifies as a taxable event as the document also says, &quot;the exchange between crypto assets having same characteristics and functions,&quot; doesn&apos;t constitute a &quot;fiscal case.&quot;</p>
+<p><br/></p>
+<p className="heading">NEWS 3</p>
+<p><br/></p>
+<p ><b>&nbsp;Microstrategy Buys More Bitcoin &mdash; Company&apos;s Crypto Holdings Grow to 132,500 BTC</b><br/><br/><br/><br></br><span className='bc'>Microstrategy is now holding approximately 132,500 bitcoin following its most recent purchases. This year, the world&rsquo;s largest cryptocurrency has emerged as &ldquo;the institutional-grade digital asset,&rdquo; said Microstrategy founder Michael Saylor.</span></p>
+<p><br/></p>
+
+<p className='p2'><b><span style={{backgroundColor:"white"}} >Microstrategy Acquires More Bitcoin</span></b></p>
+<p className='p2'  style={{paddingTop:0}}><p className='yo'>Nasdaq-listed Microstrategy Inc. announced Wednesday that it has purchased more bitcoin for its corporate treasury. Michael Saylor, the company&rsquo;s founder and executive chairman, tweeted Wednesday:</p></p>
+
+<span className='bc' >Microstrategy has increased its bitcoin holdings by ~2,500&nbsp;<a href="https://markets.bitcoin.com/crypto/BTC">BTC</a>. As of 12/27/22 Microstrategy holds ~132,500 bitcoin acquired for ~$4.03 billion at an average price of ~$30,397 per bitcoin.</span>
+<div className='p3'><p className='yo'>In its&nbsp;<a href="https://www.microstrategy.com/content/dam/website-assets/collateral/financial-documents/financial-document-archive/form-8-k-12-28-2022.pdf"> <div className='blue'>filing</div></a> with the U.S. Securities and Exchange Commission (SEC), Microstrategy clarified that the 2,500 coins were acquired between Nov. 1 and Dec. 24 by its wholly owned subsidiary Macrostrategy.</p>
+<br/>
+<br/>
+<br/>
+
+<p className='yo'>The bitcoin sale in December marked the first time Microstrategy sold its bitcoin since it began&nbsp;<a href="https://news.bitcoin.com/billion-dollar-public-company-microstrategy-250-million-btc-bitcoin-superior-to-cash/"><div className='blue'>accumulating</div></a> the crypto for its corporate treasury in 2020. The company cited tax reasons for selling&nbsp;<a href="https://markets.bitcoin.com/crypto/BTC"><div className='blue'>BTC</div></a>. &ldquo;Microstrategy plans to carry back the capital losses resulting from this transaction against previous capital gains, to the extent such carrybacks are available The subsidiary bought approximately 2,395&nbsp;<a href="https://markets.bitcoin.com/crypto/BTC"><div className='blue'>BTC</div></a> at an average price of $17,871 per coin between Nov. 1 and Dec. 21. It then sold about 704&nbsp;<a href="https://markets.bitcoin.com/crypto/BTC"><div className='blue'>BTC</div></a> at $16,776 per coin on Dec. 22. Macrostrategy then bought about 810 more&nbsp;<a href="https://markets.bitcoin.com/crypto/BTC"><div className='blue'>BTC</div></a> at $16,845 per coin on Dec. 24. At the time of writing, bitcoin is trading at $16,656.80.</p>
+<p>under the federal income tax laws currently in effect, which may generate a tax benefit,&rdquo; the SEC filing describes.</p>
+<p>Saylor said in an&nbsp;<a href="https://youtu.be/BiTc3tkrcEw?t=2"><div className='blue'>interview</div></a> last week:</p></div>
+<span className='bc' >The most positive thing of the year is the emergence of bitcoin as the institutional grade digital asset and the clarity that &hellip; there is one crypto asset that&rsquo;s a digital commodity &hellip; Bitcoin is that commodity.</span>
+<div className='p2'><p className='yo' >The executive noted that both the&nbsp;<a href="https://news.bitcoin.com/sec-chair-gensler-bitcoin-is-a-commodity/">chairman of the SEC</a> and the chair of the Commodity Futures Trading Commission (CFTC) have confirmed that&nbsp;<a href="https://news.bitcoin.com/cftc-chairman-confirms-bitcoin-ether-are-commodities/">bitcoin is a commodity</a>.</p>
+<p className='yo'>Saylor previously&nbsp;<a href="https://news.bitcoin.com/microstrategy-ceo-expects-bitcoin-to-go-into-the-millions-despite-crypto-market-sell-off/">explained</a> that Microstrategy is investing in&nbsp;<a href="https://markets.bitcoin.com/crypto/BTC">BTC</a> &ldquo;for the long term.&rdquo; He added: &ldquo;Our strategy is to buy bitcoin and hold the bitcoin, so there&rsquo;s no price target &hellip; I expect bitcoin is going to go into the millions. So, we&rsquo;re very patient. We think it&rsquo;s the future of money.&rdquo;</p>
+<p >&nbsp;</p>
+<p className='heading' style={{backgroundColor:"white"}} >News 4</p>
+<p className='yo'>Bitcoin Miners&rsquo; Revenue Hits $9.55 Billion in 2022</p>
+<p className='yo'><img src={w4} width="602" height="451" alt='w4'/><br/>2022 has been a rough and tough year for investors, traders, cryptocurrency businesses, and miners in general. The prolonged bear market, exacerbated by a series of catastrophic events, posed a threat to miners.</p>
+<p className='yo'>Miners were dealing with a variety of issues, including defaulted loans, quarterly losses, and soaring bitcoin hashrates that reached all-time highs. All these factors were affecting the profitability of the miners and were causing sell pressure.</p>
+<p className='yo'>The situation was aggravated when cryptocurrency prices, including Bitcoin&rsquo;s, plummeted. Bitcoin, which was hovering around $48,000 last year, hit a low of $15,590 by the end of 2022. According to the data collected from The Block, bitcoin miners&rsquo; revenue hit $9.55 billion in 2022.</p>
+<p className='yo'>Bitcoin miners&rsquo; revenue dropped in 2022</p>
+<p className='yo'>Bitcoin, which is trading at $16,721 at press time, has been on a downward spiral alongside other cryptocurrencies. The falling cryptocurrency prices had a major effect on the income and profits of miners. Even though the $9.55 billion in revenue in 2022 seems like a large number, it is comparatively lower than that of 2021.</p>
+<p>&nbsp;</p>
+<p className='yo'>Over the course of 2021, bitcoin miners made more than $15 billion in revenue, according to the data from the block. This was actually considered to be a 206% year-over-year increase.</p></div>
+<p className="heading">NEWS 5<br/><br/>Mango Markets Exploiter Eisenberg Arrested in Puerto Rico<br/><br/></p>
+<p>The investor drained $110 million in cryptocurrencies from the platform.</p>
+<p><img src={w5} width="582" height="350" alt='w5'/></p>
+<p><br/></p>
+<p>Avraham Eisenberg, the crypto investor whose &ldquo;<a href="https://www.coindesk.com/markets/2022/11/22/mango-exploiter-gets-liquidated-after-roiling-aave-using-20m-of-borrowed-curve-tokens/">highly profitable trading strategy</a>&rdquo; drained DeFi trading platform Mango Markets of crypto worth $110 million, was &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; arrested Monday in Puerto Rico, court documents said.</p>
+<p>The self-described game theorist admitted his role in draining Mango Markets&rsquo; treasury shortly after the incident in mid-October, and may now be the first U.S. resident to face charges for his role in manipulating a&nbsp;<a href="https://www.coindesk.com/learn/what-is-defi/">decentralized-finance</a> trading platform.</p>
+<p>Eisenberg faces charges of commodities fraud and commodities manipulation, according to&nbsp;<a href="https://storage.courtlistener.com/recap/gov.uscourts.nysd.591629/gov.uscourts.nysd.591629.1.0.pdf">a filing unsealed Tuesday</a>. The charges could see punishments ranging from fines to prison time.</p>
+<p>A deposition signed by FBI Special Agent Brandon Racz alleges Eisenberg &ldquo;willfully and knowingly&rdquo; manipulated the sale of a commodity &ndash; namely futures contracts on Mango Markets.</p>
+<p>&ldquo;Eisenberg engaged in a scheme involving the intentional and artificial manipulation of the price of perpetual futures contracts on a cryptocurrency exchange called Mango Markets, and other manipulative and deceptive devices and contrivances.&rdquo;</p>
+<p><br/></p>
+<p><br/></p>
     </div>
   );
 }
 
 export default App;
+
+
